53d0038551c8daf51ae3e0e6b05a005404e1dd7c
Sivakar Sithamparanathan <sivakar12@outlook.com>
2022-12-21 21:31:19 +0530

    Add firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import styled from "styled-components"
 import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import Sentiment from 'sentiment'
+
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 import Input from "./Input"
 import List from "./List"
@@ -9,10 +11,27 @@ import useColorScheme from "./useColorScheme"
 import * as theme from './theme'
 import Results from './Results'
 import Config from './Config'
+import { AuthProvider, AuthContext } from './firebase'
+
+const TopBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${props => props.theme.colorPalette.background2};
+`
+
+const TopBarButton = styled.div`
+  background-color: ${props => props.theme.colorPalette.background2};
+  border: none;
+  color: ${props => props.theme.colorPalette.text};
+  padding: ${props => props.theme.padding};
+  cursor: pointer;
+`
+
 
 const Title = styled.h1`
   text-align: center;
-  padding: ${props => props.theme.padding};
   font-size: ${props => props.theme.fontSizes[0]};
   background-color: ${props => props.theme.colorPalette.background2};
   color: ${props => props.theme.colorPalette.text};
@@ -38,7 +57,7 @@ function App() {
   const [state, setState] = useState(states[0]);
   const [list, setList] = useState([]);
   const [results, setResults] = useState({});
-  
+
   const colorScheme = useColorScheme();
   
   const themeWithSchemeSelected = {
@@ -61,6 +80,17 @@ function App() {
     if (state === "IN_PROGRESS" || state === "NOT_STARTED") {
       setList([...list, value]);
     }
+  }
+
+  const handleLogin = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  }
+
+  const handleLogout = () => {
+    const auth = getAuth();
+    auth.signOut();
   }
 
   useEffect(() => {
@@ -86,17 +116,36 @@ function App() {
   // eslint-disable-next-line
   }, [state])
 
+  const LoginButtonArea = () => {
+    const { currentUser } = useContext(AuthContext);
+    if (currentUser) {
+      return (
+        <TopBarButton onClick={handleLogout}>Logout</TopBarButton>
+      )
+    } else {
+      return (
+        <TopBarButton onClick={handleLogin}>Login</TopBarButton>
+      )
+    }
+  }
+
   return (
-    <ThemeProvider theme={themeWithSchemeSelected}>
-      <GlobalStyle/>
-      <Container id="container">
-        <Title>Free Association</Title>
-        { state === "NOT_STARTED" && <Config/> }
-        { state === "IN_PROGRESS" && <List list={list}/> }
-        { state === "COMPLETED" && <Results results={results}/> }
-        <Input state={state} onInput={handleInput}/>
-      </Container>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={themeWithSchemeSelected}>
+        <GlobalStyle/>
+        <Container id="container">
+          <TopBar>
+            <TopBarButton>History</TopBarButton>
+            <Title>Free Association</Title>
+            <LoginButtonArea/>
+          </TopBar>
+          { state === "NOT_STARTED" && <Config/> }
+          { state === "IN_PROGRESS" && <List list={list}/> }
+          { state === "COMPLETED" && <Results results={results}/> }
+          <Input state={state} onInput={handleInput}/>
+        </Container>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB79ISi0aNIfkcypDAFK5Q7oI34dyAjFkY",
+  authDomain: "free-association-5ffc4.firebaseapp.com",
+  projectId: "free-association-5ffc4",
+  storageBucket: "free-association-5ffc4.appspot.com",
+  messagingSenderId: "312943250102",
+  appId: "1:312943250102:web:1351ad19b99a10eb0e3417",
+  measurementId: "G-XCH9DNCPSC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+const auth = getAuth();
+
+const AuthContext = React.createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
